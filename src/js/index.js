93e541bfd8f7ea6eda1e9a288ad81aed4ba5c9cb
adkarras <chw111111@hotmail.com>
2019-08-23 13:23:02 -0600

    Added Steam Widget

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 require.context('..', true, /\.(html)$/i);
 require.context('../css', true, /\.(css)$/);
-require.context('../images', true, /\.(png)$/);
+require.context('../images', true, /\.(png|gif|jpg)$/);
